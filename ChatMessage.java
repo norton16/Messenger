820a16f8602ae5df41dc1398e1b2252b240db4c8
Brian Norton <33581086+norton16@users.noreply.github.com>
2018-11-12 20:18:49 -0500

    Add files via upload

--- ChatMessage.java
+++ ChatMessage.java
@@ -1,5 +1,4 @@
 package chatapplication;
-
 import java.io.Serializable;
 
 final class ChatMessage implements Serializable {
@@ -7,4 +6,39 @@ final class ChatMessage implements Serializable {
 
     // Here is where you should implement the chat message object.
     // Variables, Constructors, Methods, etc.
+
+    private int messageType;
+    private String message;
+    private String recipient;
+
+    public final static int broadcast = 0;
+    public final static int logout = 1;
+    public final static int dm = 2;
+    public final static int list = 3;
+
+
+    public ChatMessage(int messageType, String message)
+    {
+        this(messageType, message, null);
+    }
+
+    public ChatMessage(int messageType, String message, String recipient)
+    {
+        this.message = message;
+        this.messageType = messageType;
+        this.recipient = recipient;
+    }
+
+    public int getMessType()
+    {
+        return messageType;
+    }
+    public String getMessage()
+    {
+        return message;
+    }
+    public String getRecipient()
+    {
+        return recipient;
+    }
 }
